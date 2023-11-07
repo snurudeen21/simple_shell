956c90539ab90987af4be2ca861dacb5a5473b3c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-07 13:25:52 -0800

    simple_shell

--- betty.c
+++ betty.c
@@ -3,13 +3,14 @@
  *main-Entry point.
  *Return: return 0 after the main function run successfully
 */
+
 int main(void)
 {
 	int a = 4;
-  int *ptr = &a;
-  *ptr = 10;
-  
-  printf(" Pointer has done its thing. a = %d", a);
-  
+	int *ptr = &a;
+	*ptr = 10;
+
+	printf(" Pointer has done its thing. a = %d", a);
+
 	return (0);
 }
