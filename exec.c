4041778da7107918f5f824a572fef7fc79145e8f
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 16:49:28 +0100

    exec

--- exec.c
+++ exec.c
@@ -1,18 +1,31 @@
 #include "shell.h"
+/**
+* _exec - execute another program in this current program
+* @args - ponier arrya to command line
+*
+* Return - 0
+*/
 
-void kash_exec(char **args) {
-    pid_t child_pid = fork();
+void _exec(char **args)
+{
+    pid_t child_id = fork();
 
-    if (child_pid == 0) {
-        execvp(args[0], args);
-        perror("kash");
+    if (child_id == 0)
+    {
+        execve(args[0], args, NULL);
+        perror("nsh");
         exit(1);
-    } else if (child_pid > 0) {
-        int status;
-        do {
-            waitpid(child_pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    } else {
-        perror("kash");
+    } 
+    else if (child_id > 0)
+    {
+        int stat;
+        do
+        {
+            waitpid(child_id, &stat, WUNTRACED);
+        } while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+    }
+    else
+    {
+        perror("nsh");
     }
 }
