a9e03041f4b9a0d97d0e1684134da68e85a78786
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 16:10:47 +0100

    get_line

--- line.c
+++ line.c
@@ -1,8 +1,14 @@
 #include "shell.h"
 
-char* kash_read_line() {
+/**
+* line - pointer array to line
+* return: line
+*/
+char* read_line()
+{
     char *line = NULL;
     size_t buflen = 0;
+    
     getline(&line, &buflen, stdin);
     return line;
 }
