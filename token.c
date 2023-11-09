5b674bf4c3c9ebb87453b7d6018202119fdc1cce
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 16:42:39 +0100

    token

--- token.c
+++ token.c
@@ -1,20 +1,30 @@
 #include "shell.h"
 
-char** kash_split_line(char *line) {
+/**
+* split_line - tokinize command prompt into an array
+* @line - pointer array to tokenize
+*
+* Return - Array containing spilt command prompt
+*/
+
+char** split_line(char *line) 
+{
     int length = 0;
-    int capacity = 16;
-    char **tokens = malloc(capacity * sizeof(char*));
+    int bufer = 16;
+    char **tokens = malloc(sizeof(char*) * bufer);
 
     char *delimiters = " \t\r\n";
     char *token = strtok(line, delimiters);
 
-    while (token != NULL) {
+    while (token != NULL) 
+    {
         tokens[length] = token;
         length++;
 
-        if (length >= capacity) {
-            capacity = (int) (capacity * 1.5);
-            tokens = realloc(tokens, capacity * sizeof(char*));
+        if (length >= bufer) 
+        {
+            bufer = (int) (bufer * 1.5);
+            tokens = realloc(tokens, bufer * sizeof(char*));
         }
 
         token = strtok(NULL, delimiters);
