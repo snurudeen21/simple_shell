e81d2267fee63ce07f0016c9a6d437695ab75a6a
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 16:33:51 +0100

    main

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include "shell.h"
 
+/**
+* main - execute line, tokens and excecute functions
+* 
+* Return: 0
+*/
+
 int main()
 {
-    while (true) {
-        printf("> ");
-        char *line = kash_read_line();
-        char **tokens = kash_split_line(line);
+    while (true) 
+    {
+        my_printf("> ");
+        char *line = read_line();
+        char **tokens = split_line(line);
 
-        if (tokens[0] != NULL) {
-            kash_exec(tokens);
+        if (tokens[0] != NULL) 
+        {
+            _exec(tokens);
         }
 
         free(tokens);
