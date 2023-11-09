22d219855550c2e9d5713f3a336ca7f0f01768fa
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 21:49:46 +0100

    line

--- line.c
+++ line.c
@@ -10,6 +10,10 @@ char *read_line(void)
 	char *line = NULL;
 	size_t buflen = 0;
 
+	if(isatty(STDIN_FILENO)
+	{
+		my_prompt("#cisfun$ ");
+	}
 	getline(&line, &buflen, stdin);
 	return (line);
 }
