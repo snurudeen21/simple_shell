51038a032ef8e53e2049bae9e7a7c34bb198faa6
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 17:42:18 +0100

    exec

--- exec.c
+++ exec.c
@@ -8,24 +8,24 @@
 
 void _exec(char **args)
 {
-    pid_t child_id = fork();
+        pid_t child_id = fork();
 
-    if (child_id == 0)
-    {
-        execve(args[0], args, NULL);
-        perror("nsh");
-        exit(1);
-    } 
-    else if (child_id > 0)
-    {
-        int stat;
-        do
+        if (child_id == 0)
         {
-            waitpid(child_id, &stat, WUNTRACED);
-        } while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
-    }
-    else
-    {
-        perror("nsh");
-    }
+                execve(args[0], args, NULL);
+                perror("nsh");
+                exit(1);
+        } 
+        else if (child_id > 0)
+        {
+                int stat;
+                do
+                {
+                        waitpid(child_id, &stat, WUNTRACED);
+                } while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+        }
+        else
+        {
+                perror("nsh");
+        }
 }
