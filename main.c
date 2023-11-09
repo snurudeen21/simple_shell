d8acd46d51cc14a4bb1207c0a8965b34ca5ec63e
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 18:18:28 +0100

    main

--- main.c
+++ main.c
@@ -8,19 +8,18 @@
 
 int main()
 {
-    while (true)
-    {
-        my_prompt("#cisfun$ ");
-        char *line = read_line();
-        char **tokens = split_line(line);
+	while (true)
+	{
+		my_prompt("#cisfun$ ");
+		char *line = read_line();
+		char **tokens = split_line(line);
+		if (tokens[0] != NULL)
+		{
+			_exec(tokens);
+		}
 
-        if (tokens[0] != NULL)
-        {
-            _exec(tokens);
-        }
-
-        free(tokens);
-        free(line);
-    }
-    return 0;
+		free(tokens);
+		free(line);
+	}
+	return 0;
 }
