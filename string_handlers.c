6cb33a71941c22f0c7d769aa6d05f8f992b527ed
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 17:15:52 +0100

    string_handlers

--- string_handlers.c
+++ string_handlers.c
@@ -1,18 +1,6 @@
 #include "shell.h"
 
 /**
-* my_prompt - print a string text to stdout
-* @prompt - pointer array to text to print
-*
-* Return: 0
-*/
-
-void my_prompt(const char *prompt)
-{
-  write(STDOUT_FILENO, prompt, strlen(prompt));
-}
-
-/**
 * _strlen - returns the length of a string
 * @s: string to be counted
 *
@@ -28,4 +16,16 @@ int _strlen(char *s)
 	s++;
 	}
 	return (count);
+}
+
+/**
+* my_prompt - print a string text to stdout
+* @prompt - pointer array to text to print
+*
+* Return: 0
+*/
+
+void my_prompt(const char *prompt)
+{
+  write(STDOUT_FILENO, prompt, _strlen(prompt));
 }
