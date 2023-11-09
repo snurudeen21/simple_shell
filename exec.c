b69dd7c6ffb73c412e6ad3962019da9440942970
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-09 09:14:35 -0800

    shell

--- exec.c
+++ exec.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 /**
 * _exec - execute another program in this current program
-* @args - ponier arrya to command line
+* @args: vector array of pointers to command
 *
-* Return - 0
+* Return: 0
 */
 
 void _exec(char **args)
@@ -11,21 +11,21 @@ void _exec(char **args)
 	pid_t child_id = fork();
 
 	if (child_id == 0)
-        {
-                execve(args[0], args, NULL);
-                perror("nsh");
-                exit(1);
-        } 
-        else if (child_id > 0)
-        {
-                int stat;
-                do
-                {
-                        waitpid(child_id, &stat, WUNTRACED);
-                } while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
-        }
-        else
-        {
-                perror("nsh");
-        }
+	{
+		execve(args[0], args, NULL);
+		perror("nsh");
+		exit(1);
+	}
+	else if (child_id > 0)
+	{
+		int stat;
+
+		do {
+			waitpid(child_id, &stat, WUNTRACED);
+		} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+	}
+	else
+	{
+		perror("nsh");
+	}
 }
