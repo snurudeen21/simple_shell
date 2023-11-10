81ab985714dc7250e343e8671fccf0b1b077832a
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-10 10:22:39 +0100

    line

--- line.c
+++ line.c
@@ -9,7 +9,19 @@ char *read_line(void)
 {
 	char *line = NULL;
 	size_t buflen = 0;
+	ssize_t bytes;
 
-	getline(&line, &buflen, stdin);
+	bytes = getline(&line, &buflen, stdin);
+	if (bytes == -1)
+	{
+		perror("Error in getline");
+		free(line)
+		exit(EXIT_FAILURE);
+	}
+	if (line[bytes - 1] == '\n')
+	{
+		line[bytes - 1] == '\0';
+	}
+	
 	return (line);
 }
