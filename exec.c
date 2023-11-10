21a6af6895358ca6c8b396d762fc0735e92df930
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-10 20:25:47 +0100

    exec

--- exec.c
+++ exec.c
@@ -6,13 +6,13 @@
 * Return: 0
 */
 
-void _exec(char **args, char **envn)
+void _exec(char **args)
 {
 	pid_t child_id = fork();
 
 	if (child_id == 0)
 	{
-		execve(args[0], args, envn);
+		execve(args[0], args, NULL);
 		perror("nsh");
 		exit(1);
 	}
