7b822db5f424c77c747a160564eb8cf92cdcf500
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-10 10:45:33 +0100

    string_handler

--- string_handlers.c
+++ string_handlers.c
@@ -19,6 +19,32 @@ int _strlen(char *s)
 }
 
 /**
+* _strdup - dupicates string from array pointer
+* @s: string to be duplicated
+*
+* Return: pointer to duplicate string
+*/
+char *_strdup(char *s)
+{
+	char *ptr;
+	int i, len;
+
+	if (s == NULL)
+	return (NULL);
+
+	len = _strlen(s);
+
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+	return (NULL);
+	for (i = 0; *s != '\0'; s++, i++)
+	ptr[i] = s[0];
+
+	ptr[i++] = '\0';
+	return (ptr);
+}
+
+/**
 * my_prompt - print a string text to stdout
 * @prompt: pointer array to text to print
 *
