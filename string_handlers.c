684461854bb55e6c492759be83c11fd8bbf11958
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-11 22:54:40 +0100

    string_handlers

--- string_handlers.c
+++ string_handlers.c
@@ -94,13 +94,34 @@ char *_strdup(char *s)
 	return (ptr);
 }
 
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: char pointer
+ * @s2: char pointer
+ *
+ * Return: difference between ascii number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int length;
+
+	for (length = 0; s1[length] != 0; length++)
+	{
+	if (s1[length] - s2[length] != 0)
+	return (s1[length] - s2[length]);
+	}
+
+	return (0);
+}
+
 /**
 * my_prompt - print a string text to stdout
 * @prompt: pointer array to text to print
 *
 * Return: 0
 */
-
 void my_prompt(char *prompt)
 {
 	write(STDOUT_FILENO, prompt, _strlen(prompt));
