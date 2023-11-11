14dd18283dd30ff2eb36dd8017a41d396c3d164e
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-11 23:15:42 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -18,7 +18,6 @@ void _exec(char **args);
 char *_strcpy(char *dest, const char *src);
 char *_strcat(char *dest, char *src);
 char *_pathfinder(char *command);
-void exit_shell(char *line, char **token);
-int _strcmp(char *s1, char *s2);
+
 
 #endif /* SHELL_H */
