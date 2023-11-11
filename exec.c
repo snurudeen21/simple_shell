8e35f008e77658adb1ce446f40d69189113be8c6
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-11 10:50:33 +0100

    exec

--- exec.c
+++ exec.c
@@ -8,24 +8,80 @@
 
 void _exec(char **args)
 {
-	pid_t child_id = fork();
+	pid_t child_id;
+	int stat;
+	struct stat st;
+	
 
+	const char *paths[] = {
+	"/bin",
+	"/sbin",
+	"/usr/bin",
+	"/usr/sbin",
+	"/usr/local/bin",
+	"/usr/local/sbin",
+	"/opt/bin",
+	"/opt/sbin"
+	};
+
+	int i = 0;
+	int found = 0;
+
+	while (paths[i] != NULL)
+	{
+		char *executable_path = malloc(sizeof(char) * (_strlen(paths[i]) + _strlen(args[0]) + 2));
+		if (executable_path == NULL)
+		{
+			perror("nsh");
+			return;
+		}
+
+		_strcpy(executable_path, paths[i]);
+		_strcat(executable_path, "/");
+		_strcat(executable_path, args[0]);
+
+		if (stat(executable_path, &st) == 0)
+		{
+			found = 1;
+			args[0] = _strdup(executable_path);
+			free(executable_path);
+			break;
+		}
+		free(executable_path);
+		i++;
+	}
+
+	if (found == 0)
+	{
+		perror("nsh: command not found");
+		free(args[0]);
+		free(args);
+		return;
+	}
+   
+
+	child_id = fork();
+
+	if (child_id == -1)
+	{
+		perror("nsh");
+		return;
+	}
 	if (child_id == 0)
 	{
-		execve(args[0], args, NULL);
-		perror("nsh");
-		exit(1);
-	}
-	else if (child_id > 0)
-	{
-		int stat;
-
-		do {
-			waitpid(child_id, &stat, WUNTRACED);
-		} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("nsh");
+		}
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		perror("nsh");
+        do {
+		waitpid(child_id, &stat, WUNTRACED);
+	} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
 	}
+
+	free(args[0]);
+	free(args);
 }
