da2b31a010bf7f1bf3cf205151f5d24f084862de
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-11 14:37:28 +0100

    path_finder

---
+++ path_finder.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+char *_pathfinder(char *command)
+{
+	struct stat st;
+	const char *paths[] = {
+		"/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin",
+		"/usr/local/sbin", "/opt/bin", "/opt/sbin", NULL
+	};
+	int i = 0;
+	char *executable_path = NULL;
+
+	while (paths[i] != NULL) {
+		executable_path = malloc(200);
+		if (executable_path == NULL) {
+			perror("nsh");
+			return NULL;
+		}
+		_strcpy(executable_path, paths[i]);
+		_strcat(executable_path, "/");
+		_strcat(executable_path, command);
+
+		if (stat(executable_path, &st) == 0) 
+		{
+			break;
+		}
+		free(executable_path);
+		executable_path = NULL;
+		i++;
+	}
+
+	if (paths[i] == NULL) {
+		perror("nsh: command not found");
+		return NULL;
+	}
+
+	return executable_path;
+}
