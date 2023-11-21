b09626296c19cb7c82e213f046acf84b6639476b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-21 14:58:34 +0100

    prompt

--- prompt.c
+++ prompt.c
@@ -1,12 +1,11 @@
 #include "shell.h"
 /**
 * my_prompt - print a string text to stdout
-* @prompt: pointer array to text to print
 *
 * Return: 0
 */
 
-void my_prompt(char *prompt)
+void my_prompt(void)
 {
-	write(STDOUT_FILENO, prompt, _strlen(prompt));
+	write(STDOUT_FILENO, "($) ", 4);
 }
