f0d2398b1c342e1a1ab55e755cfb96aedb2c8fda
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-21 15:26:04 +0100

    prompt

--- prompt.c
+++ prompt.c
@@ -8,7 +8,7 @@
 void my_prompt(void)
 {
 	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
-	flags.interactive = 1;
-	if (flags.interactive)
+	flags.interact = 1;
+	if (flags.interac)
 	write(STDOUT_FILENO, "($) ", 4);
 }
