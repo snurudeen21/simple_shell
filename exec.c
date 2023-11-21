7be695ae2544acffc0feb9033754e4edfc1773cb
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-21 13:48:51 +0100

    exec

--- exec.c
+++ exec.c
@@ -10,34 +10,17 @@ void _exec(char **args)
 {
 	pid_t child_id;
 	int _stat;
-	char *executable_path;
-	struct stat st;
-
-	if (strcspn(args[0], "/") == 0)
-	{
-		executable_path = args[0];
-		if (stat(executable_path, &st) == -1)
-		executable_path = NULL;
-	}
-	else
-	executable_path = _pathfinder(args[0]);
-
-	if (executable_path == NULL)
-	{
-		free(executable_path);
-		return;
-	}
 
 	child_id = fork();
 	if (child_id == -1)
 	{
 		perror("nsh");
-		free(executable_path);
+		free(args[0]);
 		return;
 	}
 	if (child_id == 0)
 	{
-		if (execve(executable_path, args, environ) == -1)
+		if (execve(args[0], args, NULL) == -1)
 		perror("nsh");
 
 		exit(EXIT_FAILURE);
@@ -47,6 +30,4 @@ void _exec(char **args)
 			waitpid(child_id, &_stat, WUNTRACED);
 		} while (!WIFEXITED(_stat) && !WIFSIGNALED(_stat));
 	}
-
-	free(executable_path);
 }
