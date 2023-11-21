b346e1f2ccec4c8375db09cfac9a3a9687acd8ae
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2023-11-21 13:51:29 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -7,10 +7,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-extern char **environ;
 
 int _strlen(char *s);
 char *_strdup(char *s);
@@ -20,10 +17,7 @@ char **split_line(char *line);
 void _exec(char **args);
 char *_strcpy(char *dest, const char *src);
 char *_strcat(char *dest, char *src);
-char *_pathfinder(char *command);
-void exit_shell(void);
 int _strcmp(char *s1, char *s2);
-void print_env(char **env);
 void free_buf(char **buf);
 
 
