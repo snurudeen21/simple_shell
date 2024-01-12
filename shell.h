656e19752f889f4e1957471243367f6981a99f1b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-01-12 13:27:15 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -10,15 +10,16 @@
 
 extern char **environ;
 
-int _strlen(char *s);
-char *_strdup(char *s);
-void my_prompt(void);
-char **split_line(char *line);
-void _exec(char **args);
-char *_strcpy(char *dest, const char *src);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-void free_buf(char **buf);
+void _exec(char **args)
+void free_buf(char **buf)
+char *read_line(void)
+void my_prompt(void)
+char **split_line(char *line)
+int _strlen(char *s)
+char *_strcpy(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
+char *_strdup(char *s)
+int _strcmp(char *s1, char *s2)
 
 
 #endif /* SHELL_H */
