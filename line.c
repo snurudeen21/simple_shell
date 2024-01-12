39f4cd42029ee466551f1b2017cee4b4f0db7aa2
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-01-12 12:57:49 +0100

    shell

---
+++ line.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+* read_line - read <stdin> using <getline>
+*
+* Return: line
+*/
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t buflen = 0;
+	ssize_t bytes;
+
+	bytes = getline(&line, &buflen, stdin);
+	if (bytes == -1)
+	{
+		if (feof(stdin))
+		{
+			my_prompt("\n");
+			exit(0);
+		}
+		else
+		{
+			perror("Error in getline");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (line[bytes - 1] == '\n')
+	{
+		line[bytes - 1] = '\0';
+	}
+
+	return (line);
+}
