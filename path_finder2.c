49b30c0420c49cea221f28346a9c60a0d912c765
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-03-01 00:05:18 +0100

    Update path_finder2.c

--- path_finder2.c
+++ path_finder2.c
@@ -1,1 +1,23 @@
-fdfyufiu
+#include "shell.h"
+/**
+* path_finder - find path to command in PATH
+* @command: command to search in PATH
+*
+* Return: executable path
+*/
+
+char *path_finder(char *command)
+{
+	struct stat st;
+	int i = 0;
+  char *executable_path = NULL;
+
+  if (stat(command, &st) == 0)
+  {
+    executable_path = _strdup(command);
+    return (executable_path);
+  }
+
+  else
+    return (NULL);
+}
